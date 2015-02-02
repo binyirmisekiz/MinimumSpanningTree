2511ea7a9a7a53e80e7f8a75a671381718e804ff
naz <binyirmisekiz@hotmail.com>
2015-02-03 01:16:42 +0200

    Create Vertex.cs

---
+++ Vertex.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimumSpanningTree
+{
+
+    class Vertex<T> where T : IComparable
+    {
+        string id;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        Vertex<T> next;
+
+        internal Vertex<T> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+        Edge<T> edgeLink;
+
+        internal Edge<T> EdgeLink
+        {
+            get { return edgeLink; }
+            set { edgeLink = value; }
+        }
+        public Vertex(string id)
+        {
+            this.id = id;
+        }
+        public override string ToString()
+        {
+            return id;
+        }
+    }
+}
